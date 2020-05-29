c2a388ccd194a13104213683e9c97dd90223fde9
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2020-05-30 00:26:59 +0530

    Updates

---
+++ precache-manifest.017a4be58c851905d637ac98cf5020bc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5971083ff71dfe570625d60f7b65c9",
+    "url": "/resume-app/index.html"
+  },
+  {
+    "revision": "96a8feb8fceaaf6694f0",
+    "url": "/resume-app/static/css/main.2b12805e.chunk.css"
+  },
+  {
+    "revision": "0a34b4c7ebb01bcbac86",
+    "url": "/resume-app/static/js/2.5d80a36d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/resume-app/static/js/2.5d80a36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a8feb8fceaaf6694f0",
+    "url": "/resume-app/static/js/main.a9fc6cb2.chunk.js"
+  },
+  {
+    "revision": "8fd7e2581f324d542b61",
+    "url": "/resume-app/static/js/runtime-main.a32f6f6d.js"
+  },
+  {
+    "revision": "d374537d3a40c32a94afc4417f2fe9f0",
+    "url": "/resume-app/static/media/hackerrank-brands.d374537d.svg"
+  },
+  {
+    "revision": "896eb39d9dfb34401a199e6c81fa01a2",
+    "url": "/resume-app/static/media/resume-app-1.896eb39d.png"
+  },
+  {
+    "revision": "f6e377ef963cb9d17bf0f37631f78fb6",
+    "url": "/resume-app/static/media/resume-app-2.f6e377ef.png"
+  },
+  {
+    "revision": "cc83b48fd9c2b23a93383f6798fc8e33",
+    "url": "/resume-app/static/media/resume-app-3.cc83b48f.png"
+  },
+  {
+    "revision": "ef3e9bb5d5da8d6df35a8148a896f676",
+    "url": "/resume-app/static/media/resume-app-4.ef3e9bb5.png"
+  },
+  {
+    "revision": "60f9657d9ad8afd35b78fcb7df55ad71",
+    "url": "/resume-app/static/media/sanjay.60f9657d.jpg"
+  },
+  {
+    "revision": "ecbbea2bbe61c4dc36667a9dde4eee25",
+    "url": "/resume-app/static/media/todo-app-1.ecbbea2b.png"
+  },
+  {
+    "revision": "f69d98311223ba392c9d7bb004a90b36",
+    "url": "/resume-app/static/media/todo-app-2.f69d9831.png"
+  },
+  {
+    "revision": "0b41d0d353540772aed980cd7648d6f9",
+    "url": "/resume-app/static/media/todo-app-3.0b41d0d3.png"
+  }
+]);
