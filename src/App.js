267b5e25fd14bab7f09460565033ba79e7ee1d37
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2020-05-30 07:38:13 +0530

    router modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 import {
   StylesProvider,
