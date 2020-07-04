61f77206b7b15bcb5e7d1e59820cc69b1619d443
sanjaysaravanan <ssaravanan6384@altimetrik.com>
2020-07-04 15:33:38 +0530

    image load test

--- src/Pages/images.js
+++ src/Pages/images.js
@@ -1,13 +1,13 @@
-import image1 from "../assets/images/APJ-Sir-Quotes-1.jpg";
-import image2 from "../assets/images/APJ-Sir-Quotes-2.jpg";
-import image3 from "../assets/images/APJ-Sir-Quotes-3.jpg";
-import image4 from "../assets/images/APJ-Sir-Quotes-4.jpg";
-import image5 from "../assets/images/APJ-Sir-Quotes-5.jpg";
-import image6 from "../assets/images/APJ-Sir-Quotes-6.jpg";
-import image7 from "../assets/images/APJ-Sir-Quotes-7.jpg";
-import image8 from "../assets/images/APJ-Sir-Quotes-8.jpg";
-import image9 from "../assets/images/APJ-Sir-Quotes-9.jpg";
-import image10 from "../assets/images/APJ-Sir-Quotes-10.jpg";
+import image1 from "../assets/images/quotesone.jpg";
+import image2 from "../assets/images/quotestwo.jpg";
+import image3 from "../assets/images/quotesthree.jpg";
+import image4 from "../assets/images/quotesfour.jpg";
+import image5 from "../assets/images/quotesfive.jpg";
+import image6 from "../assets/images/quotessix.jpg";
+import image7 from "../assets/images/quotesseven.jpg";
+import image8 from "../assets/images/quoteseight.jpg";
+import image9 from "../assets/images/quotesnine.jpg";
+import image10 from "../assets/images/quotes-10.jpg";
 
 export const quotesList = [
   image1,
